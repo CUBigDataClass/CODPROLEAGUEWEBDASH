a035ae1d8044d8944fe2369289038870ddac6dfe
Wayne <wayne.wood@colorado.edu>
2021-03-07 22:14:07 -0700

    Cronjob, Yelp request, and S3 bucket put

--- trip-planner/routes/api.js
+++ trip-planner/routes/api.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const request = require('request');
+require('dotenv').config();
 
 router.get('/hello', (req, res) => {
     res.send({ express: 'Hello From Express' });
@@ -9,6 +11,29 @@ router.post('/world', (req, res) => {
     res.send(
         `I received your POST request. This is what you sent me: ${req.body.post}`,
     );
+});
+
+router.get('/yelp', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://api.yelp.com/v3/businesses/search',
+        qs: {
+            location: 'Denver',
+            limit: '1',
+            sort_by: 'rating'
+        },
+        headers: {
+          'Authorization': 'Bearer ' + process.env.YELP_API_KEY
+        }
+    };
+
+    // send request to API
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        // res.render(path.join(__dirname, '../client/index.ejs'), {res_arr: res_arr});
+    });
 });
 
 module.exports = router;
