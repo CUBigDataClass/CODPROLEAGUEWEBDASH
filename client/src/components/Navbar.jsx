0bf566fd27770ae5e82910502b98fdaf38da0540
artem690 <49416764+artem690@users.noreply.github.com>
2021-04-14 17:18:46 -0600

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import Navbar from 'react-bootstrap/navbar';
 import Logo  from '../images/logo2.png'
 import Styles from '../styles/Navbar.module.css'
 
 const MyNavBar = () => {
    return (
-      <Navbar className={Styles.navtheme}>
+      <div className={Styles.navtheme}>
          <img className={Styles.logo} src={Logo} alt="" />
          <h3 className={Styles.text}>Trip Planner</h3>
-      </Navbar>
+      </div>
    );
  };
  
